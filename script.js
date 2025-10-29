5f9cd3caf9e01a78964266bd6e00de65d587ca85
andersoncandid <190989879+andersoncandid@users.noreply.github.com>
2025-10-29 08:40:56 -0300

    Finish the project

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
 // Take the User choice
-function humanChoice() {
+function getHumanChoice() {
   let userAnswer = prompt("Choice: ");
-  return userAnswer.toLowerCase();
+  if (userAnswer.toLowerCase() === "rock") {
+    return 0;
+  }
+  if (userAnswer.toLowerCase() === "paper") {
+    return 1;
+  }
+  return 2; // scissors
 }
 
 // Randomly generate computer`s choice
-// 0 == rock, 1 == paper and 2 == scissors
-function computerChoice() {
+function getComputerChoice() {
   return Math.floor(Math.random() * 3);
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === 0) {
+    if (computerChoice === 1) {
+      computerScore += 1;
+    }
+    if (computerChoice === 2) {
+      humanScore += 1;
+    }
+  }
+  if (humanChoice === 1) {
+    if (computerChoice === 2) {
+      computerScore += 1;
+    }
+    if (computerChoice === 0) {
+      humanScore += 1;
+    }
+  }
+  if (humanChoice === 2) {
+    if (computerChoice === 0) {
+      computerScore += 1;
+    }
+    if (computerChoice === 1) {
+      humanScore += 1;
+    }
+  }
+  console.log("Score:");
+  console.log("Human: ", humanScore, "Computer: ", computerScore);
+}
+
+let plays = 0;
+
+// Run 5 Rounds
+do {
+  playRound(getHumanChoice(), getComputerChoice());
+  plays++;
+} while (plays < 5);
+
+console.log("");
+console.log("---Results---");
+
+if (humanScore > computerScore) {
+  console.log("Winner: Human");
+} else if (humanScore < computerScore) {
+  console.log("Winner: Computer");
+} else {
+  console.log("Draw!");
+}
